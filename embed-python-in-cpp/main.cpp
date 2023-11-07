54d1305b63c56689f20b1640ceaf5b789fc04bee
Midhun A Darvin <midhunadarvin@gmail.com>
2023-11-07 16:36:20 +0530

    feat: call python function via design by contract

--- embed-python-in-cpp/main.cpp
+++ embed-python-in-cpp/main.cpp
@@ -2,21 +2,48 @@
  * g++ -o main.exe $(python3-config --cflags) main.cpp /opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/Current/lib/libpython3.11.dylib
  * Compile: g++ main.cpp -c $(python3-config --cflags) -fPIC
  * Link   : g++ main.o $(python3-config --ldflags) -o main.exe
- * Run    : main.exe multiply multiply <arg-1> <arg-2>
-*/
+ * Run    : main.exe multiply <arg-1> <arg-2>
+ */
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <iostream>
 
-int
-main(int argc, char *argv[])
+PyObject *createExecutionContext(char *a, char *b)
+{
+    // Create a Python dictionary
+    PyObject *dict = PyDict_New();
+    if (!dict)
+    {
+        PyErr_Print();
+        return nullptr;
+    }
+
+    // Add key-value pairs to the dictionary
+    PyObject *key1 = PyUnicode_DecodeUTF8("key1", 4, "strict");
+    PyObject *value1 = PyUnicode_DecodeUTF8(a, strlen(a), "strict");
+    PyDict_SetItem(dict, key1, value1);
+
+    PyObject *key2 = PyUnicode_DecodeUTF8("key2", 4, "strict");
+    PyObject *value2 = PyUnicode_DecodeUTF8(b, strlen(b), "strict");
+    PyDict_SetItem(dict, key2, value2);
+
+    // Print the dictionary
+    PyObject_Print(dict, stdout, 0);
+    printf("\n");
+    return dict;
+}
+
+int main(int argc, char *argv[])
 {
     wchar_t *program = Py_DecodeLocale(argv[0], NULL);
-    if (program == NULL) {
+    if (program == NULL)
+    {
         fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
         exit(1);
     }
 
-    PyObject *pName, *pModule, *pFunc;
+    PyObject *pName, *pModule;
+    PyObject *preExecuteFunc, *executeFunc, *postExecuteFunc;
     PyObject *pArgs, *pValue;
 
     // Initialize python
@@ -31,53 +58,75 @@ main(int argc, char *argv[])
     pModule = PyImport_Import(pName);
     Py_DECREF(pName);
 
-    if (pModule != NULL) {
-        pFunc = PyObject_GetAttrString(pModule, argv[2]);
-        /* pFunc is a new reference */
+    if (pModule != NULL)
+    {
 
-        if (pFunc && PyCallable_Check(pFunc)) {
-            pArgs = PyTuple_New(argc - 3);
-            for (int i = 0; i < argc - 3; ++i) {
-                pValue = PyLong_FromLong(atoi(argv[i + 3]));
-                if (!pValue) {
-                    Py_DECREF(pArgs);
-                    Py_DECREF(pModule);
-                    fprintf(stderr, "Cannot convert argument\n");
-                    return 1;
+        preExecuteFunc = PyObject_GetAttrString(pModule, "preExecute");
+        executeFunc = PyObject_GetAttrString(pModule, "execute");
+        postExecuteFunc = PyObject_GetAttrString(pModule, "postExecute");
+
+        PyObject *executionContext = createExecutionContext(argv[2], argv[3]);
+        PyObject *pArgs = PyTuple_New(1);
+        PyTuple_SetItem(pArgs, 0, executionContext);
+        if (preExecuteFunc && PyCallable_Check(preExecuteFunc))
+        {
+            PyObject *preExecuteResult = PyObject_CallObject(preExecuteFunc, pArgs);
+            if (preExecuteResult != NULL)
+            {
+                std::cout << "PreExecute :" << Py_IsTrue(preExecuteResult) << std::endl;
+                if (Py_IsTrue(preExecuteResult))
+                {
+                    if (executeFunc && PyCallable_Check(executeFunc))
+                    {
+                        PyObject *executeFuncResult = PyObject_CallObject(executeFunc, pArgs);
+                        std::cout << "Execute :" << Py_IsTrue(executeFuncResult) << std::endl;
+                        Py_DECREF(executeFuncResult);
+                    }
+                    else
+                    {
+                        if (PyErr_Occurred())
+                            PyErr_Print();
+                        fprintf(stderr, "Cannot find function \"%s\"\n", "preExecute");
+                        Py_DECREF(executeFunc);
+                        Py_DECREF(pModule);
+                    }
+
+                    if (postExecuteFunc && PyCallable_Check(postExecuteFunc))
+                    {
+                        PyObject *postExecuteFuncResult = PyObject_CallObject(postExecuteFunc, pArgs);
+                        std::cout << "PostExecute :" << Py_IsTrue(postExecuteFuncResult) << std::endl;
+                        Py_DECREF(postExecuteFuncResult);
+                    }
+                    else
+                    {
+                        if (PyErr_Occurred())
+                            PyErr_Print();
+                        fprintf(stderr, "Cannot find function \"%s\"\n", "preExecute");
+                        Py_DECREF(postExecuteFunc);
+                        Py_DECREF(pModule);
+                    }
                 }
-                /* pValue reference stolen here: */
-                PyTuple_SetItem(pArgs, i, pValue);
-            }
-
-            pValue = PyObject_CallObject(pFunc, pArgs);
-            Py_DECREF(pArgs);
-            if (pValue != NULL) {
-                printf("Result of call: %ld\n", PyLong_AsLong(pValue));
-                Py_DECREF(pValue);
-            }
-            else {
-                Py_DECREF(pFunc);
-                Py_DECREF(pModule);
-                PyErr_Print();
-                fprintf(stderr,"Call failed\n");
-                return 1;
+                Py_DECREF(preExecuteResult);
             }
         }
-        else {
+        else
+        {
             if (PyErr_Occurred())
                 PyErr_Print();
-            fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+            fprintf(stderr, "Cannot find function \"%s\"\n", "preExecute");
         }
-        Py_XDECREF(pFunc);
+
         Py_DECREF(pModule);
     }
-    else {
+    else
+    {
         PyErr_Print();
         fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
         return 1;
     }
 
-    if (Py_FinalizeEx() < 0) {
+    if (Py_FinalizeEx() < 0)
+    {
         exit(120);
     }
     PyMem_RawFree(program);
--- embed-python-in-cpp/multiply.py
+++ embed-python-in-cpp/multiply.py
@@ -1,6 +1,15 @@
-def multiply(a, b):
-    print("Will compute", a, "times", b)
+def preExecute(context):
+    return True
+
+def execute(context):
+    print("Will compute", context["key1"], "times", context["key2"])
+    a = int(context["key1"])
+    b = int(context["key2"])
     c = 0
     for i in range(0, a):
         c = c + b
-    return c
+    print(c)
+    return True
+
+def postExecute(context):
+    return True
